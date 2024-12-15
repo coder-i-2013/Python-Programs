a19addadcb5e11d1b5c662d9658c63de4f34e455
Aishwarya srivastava <Ahanajanvi@gmail.com>
2024-12-15 07:54:36 -0500

    Update birthdays.py

--- birthdays.py
+++ birthdays.py
@@ -1,8 +1,22 @@
 import datetime
-name="ahana"
+name="Ahana"
 Ahana_date="20"
 Ahana_month="May"
 Ahana_year="2013"
 print(name,Ahana_date,Ahana_month,Ahana_year,sep="/")
-x= datetime.datetime(2013,5,20)
+print("Priyadarshini")
+x= datetime.datetime(1984,5,8)
 print(x)
+print("Abhishek")
+y= datetime.datetime(1984,5,20)
+print(y)
+namel="Alisha"
+Ahanal_date="18"
+Ahanal_month="January"
+Ahanal_year="2015"
+print(namel,Ahanal_date,Ahanal_month,Ahanal_year,sep="/")
+namelb="Aayush"
+Ahanalb_date="23"
+Ahanalb_month="April"
+Ahanalb_year="2023"
+print(namelb,Ahanalb_date,Ahanalb_month,Ahanalb_year,sep="/")
