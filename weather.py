1a5f5b7b5757883db043e8fdd16e50a2f1196647
Aishwarya srivastava <Ahanajanvi@gmail.com>
2025-01-02 16:24:36 -0500

    what should/ could you wear

---
+++ weather.py
@@ -0,0 +1,13 @@
+temp=float(input("Enter the temprature at your location in farhanite:  "))
+if temp <= 30:
+    print("The weather is below freezing you should wear thick layers. ")
+elif temp <= 40:
+    print("The weather is cold you could wear pants and a jacket . ")
+elif temp <= 50:
+    print("The weather is below cool you could wear pants and a t-shirt. ")
+elif temp <= 60:
+    print("The weather is below nice you could wear shorts and a tshirt . ")
+elif temp >= 70:
+    print("The weather is below warm/hot you could wear shorts and a tank top. ")
+
+
