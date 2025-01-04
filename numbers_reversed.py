ad2f910e19e4837cc396cd83d46c49af6ad9ba88
Aishwarya srivastava <Ahanajanvi@gmail.com>
2025-01-04 08:44:31 -0500

    for loops

---
+++ numbers_reversed.py
@@ -0,0 +1,5 @@
+n=int(input("Enter the value for N; "))
+print("the numbers from {0} to {1} are:".format(n,1))
+for i in range(n,0,-1):
+    print(i)
+
---
+++ str_reverse.py
@@ -0,0 +1,7 @@
+string=input("Please enter a string that you wish to reverse: ")
+string2=('')
+for i in string:
+    string2=i+string2
+print("The orignal string is",string)
+print("The reversed string is",string2)
+
