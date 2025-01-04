8f7d2dba05ed14a10c3153f775d7aff371eccf84
Aishwarya srivastava <Ahanajanvi@gmail.com>
2025-01-04 08:56:19 -0500

    powers and squares

---
+++ powers.py
@@ -0,0 +1,4 @@
+n=int(input("enter a number: "))
+power=int(input("Enter a power: "))
+
+print(f"{n} raised to {power} is", n**power)
---
+++ squareroot.py
@@ -0,0 +1,7 @@
+import math
+
+
+number = float(input("Enter any number:"))
+square_root = math.sqrt(number)
+
+print(square_root)
