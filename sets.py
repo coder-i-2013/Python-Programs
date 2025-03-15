3db92115dd02b3f6fd557008a595c4c2c39fe5bc
Aishwarya srivastava <Ahanajanvi@gmail.com>
2025-03-15 10:58:07 -0400

    sets and arrays

---
+++ sets.py
@@ -0,0 +1,20 @@
+set_a={1,2,3}
+print(set_a)
+
+# New Set
+set_b={1.0,"hello",1,2,3}
+print(set_b)
+# New Set
+set_c={1,1,1,2,3,3,4,5}
+print(set_c)
+# New Set
+set_d=set(["Namaste","Hello","Namaste","Bonjour","Hola"])
+print(set_d)
+# New Set
+set_e=set(["123","126","122","124","125","122"])
+print(set_e)
+set_e.pop()
+print("this is what  ")
+print(set_e)
+
+
