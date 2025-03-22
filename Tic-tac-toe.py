48f6eb210f46ad8dec7fefbec521d13ca2e12ade
Aishwarya srivastava <Ahanajanvi@gmail.com>
2025-03-22 10:57:56 -0400

    Tic-Tac_toe

---
+++ Tic-tac-toe.py
@@ -0,0 +1,88 @@
+theBoard={'7':' ','8':' ','9':' ',
+          '4':' ','5':' ','6':' ',
+          '1':' ','2':' ','3':' '}
+
+board_keys=[]
+
+for key in theBoard:
+    board_keys.append(key)
+
+def print_board(board):
+    print(board['7']+ '|' + board['8']+ '|'+ board['9'])
+    print("-+-+-")
+    print(board['4']+ '|' + board['5']+ '|'+ board['6'])
+    print("-+-+-")    
+    print(board['1']+ '|' + board['2']+ '|' + board['3'])
+
+def game():
+    turn='X'
+    count=0
+    for i in range (10):
+        print_board(theBoard)
+        print("it is your turn",turn,"move to which place: ")
+        move=input()
+        if theBoard[move]==" ":
+            theBoard[move]=turn
+            count+=1
+        else:
+            print("The place you selected is filled where do you want move?: ")
+
+            continue
+        if count>= 5:
+            if theBoard['7'] == theBoard['8'] == theBoard['9'] != ' ':
+                print_board(theBoard)
+                print("Game over")
+                print(turn +"won")
+                break
+            elif theBoard['4'] == theBoard['5'] == theBoard['6'] != ' ':
+                print_board(theBoard)
+                print("Game over")
+                print(turn +"won")
+                break
+            elif theBoard['1'] == theBoard['2'] == theBoard['3'] != ' ':
+                print_board(theBoard)
+                print("Game over")
+                print(turn +"won")
+                break
+            elif theBoard['1'] == theBoard['4'] == theBoard['7'] != ' ':
+                print_board(theBoard)
+                print("Game over")
+                print(turn +"won")
+                break
+            elif theBoard['2'] == theBoard['5'] == theBoard['8'] != ' ':
+                print_board(theBoard)
+                print("Game over")
+                print(turn +"won")
+                break
+            elif theBoard['3'] == theBoard['6'] == theBoard['9'] != ' ':
+                print_board(theBoard)
+                print("Game over")
+                print(turn +"won")
+                break
+            elif theBoard['7'] == theBoard['5'] == theBoard['3'] != ' ':
+                print_board(theBoard)
+                print("Game over")
+                print(turn +"won")
+                break
+            elif theBoard['1'] == theBoard['5'] == theBoard['9'] != ' ':
+                print_board(theBoard)
+                print("Game over")
+                print(turn +"won")
+                break
+        if count==9:
+            print("Game Over")
+            print("IT IS A TIE")
+        if turn == 'X':
+            turn='O'
+        else:
+            turn='X'
+    restart= input("Do you want to play again (y/n): ")
+    
+    if restart == 'y' or restart=='Y':
+        for key in theBoard:
+            theBoard[key]=' '
+        game()    
+if __name__=="__main__":
+    game()
+    
+
