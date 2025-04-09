c9d90ef0205f1dc9f0d42ff57e7c979b3e1ae6f3
Aishwarya srivastava <Ahanajanvi@gmail.com>
2025-04-09 14:38:12 -0400

    perimeter and area circles

---
+++ circles_p_and_a.py
@@ -0,0 +1,8 @@
+import math
+
+radius = float(input("Enter the radius of the circle: "))
+area = math.pi * radius**2
+perimeter = 2 * math.pi * radius
+
+print("Area:", area)
+print("Perimeter:", perimeter)
