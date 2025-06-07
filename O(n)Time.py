31c988541b4adfc9920e7f3d079b4c25876513cd
Aishwarya srivastava <Ahanajanvi@gmail.com>
2025-06-07 10:56:38 -0400

    Asymptotic analysis

---
+++ O(n)Time.py
@@ -0,0 +1,11 @@
+def Ontime(n):
+    iteration=0
+    for i in range (1,n+1):
+        iteration+=1
+    print("when n is ",n,"Iterations=",iteration)
+
+Ontime(10)
+Ontime(20)
+Ontime(42)
+print("With every iteration the time taken will increase")
+
---
+++ o(1)time.py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iteration=0
+    print("the number entered is",n)
+    iteration+=1
+    print("The total iterations done by this code is:",iteration,"\n")
+
+printnumber(20)
+printnumber(40)
+print("\nWith anu 'n'the time taken bu=y our code will not change")
